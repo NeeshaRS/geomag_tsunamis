13050b9f7f4d07abbe0d896bfc6944044c39d62c
Neesha Schnepf <neesha.schnepf@lasp.colorado.edu>
2022-01-19 10:51:45 -0700

    plotting H and Z fields

---
+++ tsunami_library/F_HZ_plot.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = F_HZ_plot(inputArg1,inputArg2)
+%F_rawHZ_plot Plot the station's horizontal and vertical magnetic fields
+% (nT).
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
