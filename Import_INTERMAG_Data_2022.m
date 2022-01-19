e4dc733bd0234065bc3ced26a831dccdab38fa95
Neesha Schnepf <neesha.schnepf@lasp.colorado.edu>
2022-01-19 10:12:13 -0700

    testing function on KAK

--- Import_INTERMAG_Data_2022.m
+++ Import_INTERMAG_Data_2022.m
@@ -1,6 +1,8 @@
 %% Clear parameters and set basics
 clc; clear all;
 
+addpath('tsunami_library')
+
 daymin = 24*60;
 daysec = daymin*60;
 
@@ -8,11 +10,27 @@ daysec = daymin*60;
 time1 = datenum('01-15-2022 00:00:00');
 timeE = datenum('01-16-2022 23:59:59');
 
+% build time arrays
 time_s = time1:1/daysec:timeE;
-disp('Time array with second steps made.')
+time_min = time1:1/daymin:timeE;
+disp('Time arrays made.')
+
+% time data for finding data files
+year = '2022';
+month = '01';
+days= 15:16;
 
 %% KAK
-addpath ../../Data/INTERMAGNET/KAK/provisional/2022/01/
+station = 'kak';
+datapath = '../../Data/INTERMAGNET/KAK/provisional/2022/01/';
+timeres = 'min';
+dtype = 'p';
+num_header = 20;
+
+[Bx, By, Bz, Bh] = F_load_INTERMAG(station, datapath, timeres, ...
+    dtype, year, month, days, num_header)
+
+addpath 
 days= 15:16;
 formatSpec = 'kak202201%dpsec.sec';
 daynum= 1;
@@ -42,3 +60,4 @@ plot(time_s, kakZ)
 
 subplot(212)
 plot(time_s, kakH)
+
