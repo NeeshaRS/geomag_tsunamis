130b3960a18a33ac2e368672748e215bf6df5326
Neesha Schnepf <neesha.schnepf@lasp.colorado.edu>
2022-01-19 12:25:55 -0700

    script for CWA of this event

---
+++ 2022_Tonga/Tonga2022_CWA.m
@@ -0,0 +1,47 @@
+%% Clear and set initial parameters
+clc; clear all; close all;
+
+Sc = 0.1:0.1:200;
+maxT=1*60*30; % .5 hours is the max period
+dt= 60; % The sample rate of one per minute
+waven = 'cgau4';
+n=7;
+daymin=24*60;
+
+addpath('../tsunami_library')
+
+%% local = KAK, remote = KNY
+load kak_2022-01-15_zh.mat
+load kny_2022-01-15_zh.mat
+whos
+
+% very rough back of the envelope estimate of tsunami arrival
+etaT= datenum('15-Jan-2022 14:00:00'); 
+
+local_Z= kakZ;
+local_H= kakH;
+remote_H= knyH;
+
+[W_local_Zw1,CWA_plot,zoom_plot, maxamp, maxtime] = ...
+F_CWA(Sc, maxT, dt, waven, n, local_Z, local_H, remote_H, time_min, etaT);
+
+%% local = MMB, remote = KNY
+
+
+%% local = CHI, remote = KNY
+
+
+%% local = KNY, remote = MMB
+
+
+%% local = CNB, remote = API
+
+
+%% local = API, remote = CNB
+
+
+%% local = IPM, remote = PPT
+
+
+%% local = PPT, remote = IPM
+
