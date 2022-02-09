d7911cb31e5dedca331fbe66a78807f673585110
Neesha Schnepf <neesha.schnepf@lasp.colorado.edu>
2022-02-09 13:50:46 -0700

    CBI water anal + some reorg

--- 2022_Tonga/Water_level_analysis.m
+++ 2022_Tonga/Water_level_analysis.m
@@ -5,9 +5,8 @@ addpath matlab_datafiles/
 addpath('../tsunami_library')
 
 % for high pass filter
-% maxT=1*60*10; % 10 min is the max period
-% maxT=1*60*30; % 30 min is the max period
-maxT=1*60*120; % 120 min is the max period
+T= 30;  % max period in minutes
+maxT=1*60*T; 
 dt= 60; % The sample rate of one per minute
 n=7;
 
@@ -15,6 +14,9 @@ disp('initial parameters set')
 %% API
 for i=1
     close all
+    station= 'api';
+    stationC= 'API';
+
     % load water level data
     load API_water_levels_m.mat
     load api_2022-01-15_zh.mat
@@ -27,23 +29,72 @@ for i=1
     % high pass filter water level data
     height_m_hpf = F_HPF(maxT, dt, n, height_m');
     % write out the water level data
-    [fid] = F_woTimeData(time,height_m_hpf, ...
-        'matlab_datafiles/api_water_levels_m_hpf_T120min.csv');
+    figname = ...
+        sprintf('matlab_datafiles/%s_water_levels_m_hpf_T%imin.csv',...
+        station, T)
+    [fid] = F_woTimeData(time,height_m_hpf, figname);
 
     % plot water level vs raw magnetic field data
-    F_waterB_plot(time,height_m_hpf, time_min2, apiZ,...
-        'figures/API/water_level_analysis/api_whpf_Z_120min.png')
-    F_waterB_plot(time,height_m_hpf, time_min2, apiH,...
-        'figures/API/water_level_analysis/api_whpf_H_120min.png')
+    figname = ...
+        sprintf('figures/%s/water_level_analysis/%s_whpf_Z_%imin.png', ...
+        stationC, station, T)
+    F_waterB_plot(time,height_m_hpf, time_min2, apiZ, figname)
+    figname = ...
+        sprintf('figures/%s/water_level_analysis/%s_whpf_H_%imin.png', ...
+        stationC, station, T)
+    F_waterB_plot(time,height_m_hpf, time_min2, apiH,figname)
 
     % high pass filter magnetic data
     apiZ_hpf = F_HPF(maxT, dt, n, apiZ);
     apiH_hpf = F_HPF(maxT, dt, n, apiH);
     
     % plot water level vs raw magnetic field data
+    figname= sprintf(...
+        'figures/%s/water_level_analysis/%s_whpf_Bhpf_%imin.png', ...
+        stationC, station, T)
     F_waterB_3plot(time,height_m_hpf, time_min2, apiZ_hpf, apiH_hpf,...
-        'figures/API/water_level_analysis/api_whpf_Bhpf_120min.png')
+        figname)
 
+end
+
+%% CBI
+for i=1
+    close all
+
+    % load water level data
+    load CBI_water_levels.mat
+    load cbi_2022-01-15_zh.mat
+    % convert time array to matlab datetime
+    time_min2= datetime(time_min, 'ConvertFrom','datenum');
+    
+    day1= datetime(2022,01,15);
+    day2= datetime(2022,01,16);
+
+    % remove NaNs from water level data
+    height_m= height_m(~isnan(height_m))';
+    time= time(~isnan(height_m));
+    height_m= height_m(time >= day1 & time < day2);
+    time= time(time >= day1 & time < day2);
+ 
+    % high pass filter water level data
+    height_m_hpf = F_HPF(maxT, dt, n, height_m');
+    % write out the water level data
+    [fid] = F_woTimeData(time,height_m_hpf, ...
+        'matlab_datafiles/cbi_water_levels_m_hpf_T120min.csv');
+
+    % plot water level vs raw magnetic field data
+    F_waterB_plot(time,height_m_hpf, time_min2, cbiZ,...
+        'figures/CBI/water_level_analysis/cbi_whpf_Z_120min.png')
+    F_waterB_plot(time,height_m_hpf, time_min2, cbiH,...
+        'figures/CBI/water_level_analysis/cbi_whpf_H_120min.png')
+
+    % high pass filter magnetic data
+    cbiZ_hpf = F_HPF(maxT, dt, n, cbiZ);
+    cbiH_hpf = F_HPF(maxT, dt, n, cbiH);
+    
+    % plot water level vs raw magnetic field data
+    F_waterB_3plot(time,height_m_hpf, time_min2, cbiZ_hpf, cbiH_hpf,...
+        'figures/CBI/water_level_analysis/cbi_whpf_Bhpf_120min.png')
 end
 
 %% HON
